6a7944e8871e7d9506ad7c6b6d2411ce59b26154
raul-ortega <raul.ya@gmail.com>
2021-11-15 12:18:33 +0100

    use rdflib serializer https://github.com/RDFLib/pyLODE/pull/154

--- pylode/profiles/base.py
+++ pylode/profiles/base.py
@@ -462,8 +462,8 @@ class BaseProfile:
 
         # make sure the json-ld serializer is registered
         from rdflib.plugin import register, Serializer
-        register('json-ld', Serializer, 'rdflib_jsonld.serializer', 'JsonLDSerializer')
-
+        #register('json-ld', Serializer, 'rdflib_jsonld.serializer', 'JsonLDSerializer')
+        register('json-ld', Serializer, 'rdflib.plugins.serializers.jsonld', 'JsonLDSerializer')
         return g.serialize(format="json-ld", encoding="utf-8")  # support >= rdflib 6.0.0 and ensure backwards compat (last python 2 release)
 
     def _make_agent_link(self, name, url=None, email=None, affiliation=None):
